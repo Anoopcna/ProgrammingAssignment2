2010fad7314dea829eec1e3af7ad2641ca48e6af
Anoop Nair <anoopcna@gmail.com>
2015-07-26 13:09:41 +0530

    The two functions have been added namely makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here are two functions makeCacheMatrix which sets the initial values,
+#assign the values of the vector,and outpting the list of functions used
+#within the main function
+#The later function cacheSolve uses the above function,to compute whether its 
+#Inverse computation for the first time or is already cached ,accordingly,
+#the 2nd function decides whether computation is required or not.
 
-## Write a short comment describing this function
+
+##########makeCacheMatrix################
+
+#i stands for the value for inverse of the matrix.
+#to set the value for the matrix and inverse.
+#to get the value for the matrix.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        i <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
+###############cacheSolve##################
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+#if the value is not null and is present alredy hence dont compute;
+#but printh result from the cache.
+#to check whether the matix is singular,accordingly either display the message of the Inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        
+        
+        if(det(data)==0)
+        {
+                message("Singular matrix hence,Inverse is not possible")
+        }
+                
+        
+        else if (det(data)!=0)
+        {
+        
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
+}
+
+        
+        
